50bb2e7e13e412b7eb4e48bdd55fecbd91a0a6cc
Obada Mahdi <omahdi@gmail.com>
2017-06-27 00:12:41 +0200

    Adding error checking macro
    - macro CHECK_GL_ERROR(), defined in happah/graphics/CheckGLError.hpp
      replaces assertions that test for GL errors
    - preprocessor macro HAPPAH_GL_ERROR_PERMISSIVE can be used to switch
      between permissive error checking (default) that only displays
      a message, and the old behavior (assert() call that will terminate on
      errors)
    - macro is evaluated as boolean; set to 0 for old behavior
    - only replaced error checks in happah/graphics/Program.cpp for now

--- lib/happah/graphics.hpp
+++ lib/happah/graphics.hpp
@@ -3,6 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include "happah/graphics/CheckGLError.hpp"
 #include "happah/graphics/DataType.hpp"
 #include "happah/graphics/Buffer.hpp"
 #include "happah/graphics/glad.h"
