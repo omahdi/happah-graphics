0b42a9f40f13922d6e3b4ebeb8fbe52e11cd8f53
Obada Mahdi <omahdi@gmail.com>
2017-09-24 12:58:56 +0200

    ENH: highlight edge shaders
    - reverting shaders/edge.?.glsl to master branch version
    - adding modified shaders as "hiedge.?.glsl"
    - generated instantiation code in Shader.hpp"

--- shaders/hiedge.g.glsl
+++ shaders/hiedge.g.glsl
@@ -18,7 +18,6 @@ layout(triangle_strip, max_vertices = 3) out;
 in Vertex {
      vec4 position;
      vec4 edgeColor;
-     vec4 vertexColor;
 } vertex_in[];
 
 out Vertex {
@@ -31,10 +30,6 @@ out Triangle {
      flat vec4 edgeColor0;
      flat vec4 edgeColor1;
      flat vec4 edgeColor2;
-     flat vec4 vertexColor0;
-     flat vec4 vertexColor1;
-     flat vec4 vertexColor2;
-     flat vec3 maxheights;
 } triangle_out;
 
 #include "/happah/geometry.h.glsl"
@@ -44,16 +39,15 @@ void main() {
      vec4 v1 = vertex_in[1].position;
      vec4 v2 = vertex_in[2].position;
      
-     vec3 heights = calc_heights(v0, v1, v2);
      vec4 normal = calc_normal(v0, v1, v2);
      
-     vertex_out.dis = vec3(0, heights.y, 0);
+     vertex_out.dis = vec3(0, 1, 0);
      vertex_out.normal = normal;
      vertex_out.position = v0;
      gl_Position = gl_in[0].gl_Position;
      EmitVertex();
 
-     vertex_out.dis = vec3(0, 0, heights.z);
+     vertex_out.dis = vec3(0, 0, 1);
      vertex_out.normal = normal;
      vertex_out.position = v1;
      gl_Position = gl_in[1].gl_Position;
@@ -62,12 +56,8 @@ void main() {
      triangle_out.edgeColor0 = vertex_in[0].edgeColor;
      triangle_out.edgeColor1 = vertex_in[1].edgeColor;
      triangle_out.edgeColor2 = vertex_in[2].edgeColor;
-     triangle_out.vertexColor0 = vertex_in[0].vertexColor;
-     triangle_out.vertexColor1 = vertex_in[1].vertexColor;
-     triangle_out.vertexColor2 = vertex_in[2].vertexColor;
-     triangle_out.maxheights = vec3(heights.x, heights.y, heights.z);
      
-     vertex_out.dis = vec3(heights.x, 0, 0);
+     vertex_out.dis = vec3(1, 0, 0);
      vertex_out.normal = normal;
      vertex_out.position = v2;
      gl_Position = gl_in[2].gl_Position;
